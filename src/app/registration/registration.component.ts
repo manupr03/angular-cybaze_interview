a580217699877df5a00a01b2fe6cf401ddad2745
ManuDev03 <manuprajan03@gmail.com>
2021-08-27 12:47:39 +0530

    saving register form to a array

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -10,6 +10,8 @@ export class RegistrationComponent implements OnInit {
 
   constructor() { }
   genders =["male","female"]
+  
+  registeredUsernames:Array<any> =['Testuser1','Testuser2']
   registerform: FormGroup;
 
   ngOnInit(): void {
@@ -23,6 +25,8 @@ export class RegistrationComponent implements OnInit {
   }
   onRegister(){
    console.log(this.registerform.value)
+   this.registeredUsernames.push(this.registerform.value)
+   console.log(this.registeredUsernames)
   }
 
 }
